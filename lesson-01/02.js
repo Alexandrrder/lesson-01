fe5bec0566bf5ebd4f9a3dedf86e0c018973b909
alexandrrder <alexandrrder@gmail.com>
2024-10-24 13:44:04 +0200

    lesson 02

---
+++ lesson-01/02.js
@@ -0,0 +1,22 @@
+/* 
+
+let sum = 0;
+let i = 1;
+
+while (i <= 20) {
+    sum += i;
+    i++;
+}
+
+*/
+
+let factorial = 1;
+
+let n = 10;
+
+let i = 1;
+
+while (i <= n) {
+    factorial * 1;
+    i++;
+}
